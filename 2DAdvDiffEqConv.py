f81fdb4402e9b126729eae66d4db25b53cc93e5f
Carmi <rcarmi@me.com>
2014-11-12 15:27:02 -0800

    Same

--- 2DAdvDiffEqConv.py
+++ 2DAdvDiffEqConv.py
@@ -11,16 +11,17 @@ import errno
 ## 2DAdvDiffEq.py
 ## Remi Carmigniani
 ## Solves the 2D Diffusion Equation :
-## pd_t u[x,y,t] + pd_x u[x,y,t] + alpha * pd_y u[x,y,t] - 1/Pe(pd_xx u[x,y,t]+pd_yy u[x,y,t])  = 0 (x,y) in [0 2pi]^2
+## pd_t u[x,y,t] + pd_x u[x,y,t] +alpha pd_y u[x,y,t] - 1/Pe(pd_xx u[x,y,t]+pd_yy u[x,y,t])  = 0 (x,y) in [0 2pi]^2
 ## BC periodic
 ## IC u[x,y,0]=cos(x)+cos(y) 
 ## Convergence test\
 
+#IMPORTANT TO COPY A MATRIX USE u_old = [row[:] for row in u_arr] this will unlink them...
 
 ##Physical parameter
 Pe = 1.0
 L=2*pi
-alpha=0.1#Keep alpha smaller than 1
+
 ##################################################################################################################
 ############################			Useful functions		      ############################
 ##################################################################################################################
@@ -55,7 +56,7 @@ def iCond(x,y):
 
 
 #Exact solution
-def u_exact(x,y,t,Pe,apha):
+def u_exact(x,y,t,Pe,alpha):
 	return exp(-t/Pe)*(.5*cos(x-t) + .5*cos(y-alpha*t))
 
 ## Create a one row vector from the matrix u_arr
@@ -74,12 +75,21 @@ def vecToMatrix(u,nx,ny):
 		for j in range(0,ny):
 			Umat[i][j]=u[i*ny+j]
     	return Umat
+#Periodic BC
+def period(k,n):
+	if k==-1:
+		return n-2
+	else:
+		if k==n:
+			return 1
+		else:
+			return k 
 ##################################################################################################################
 ############################				End			      ############################
 ##################################################################################################################
 #Simulation parameters
 tend = 0.5
-
+alpha=0
 
 error=[]
 ## Series of N to try
@@ -93,7 +103,7 @@ for k in range(0,3):
 	dy=L/float(Ny-1)
 
 	## time parameters 
-	dt =0.01*dx*dx
+	dt =0.1*dx*dx
 	#to make sure tend is reached and stability ok
 	#dt = tend/float(int(tend/dt)+1)
 
@@ -101,11 +111,9 @@ for k in range(0,3):
 	# Laplacian terms
 	lx = 1.*dt/dx/dx
 	ly=1.*dt/dy/dy
-	# Adv terms
-	ax =.5*dt/dx
-	ay=.5*dt/dy*alpha
-
-
+	ax = 0.5*dt/dx
+	ay = 0.5*alpha*dt/dx
+	
 	## Figure numbering 
 	numb = 0
 	uval = iCond(0,0)
@@ -121,10 +129,10 @@ for k in range(0,3):
 	#for i in range(0,Nx):
 	#	for j in range(0,Ny):      
 	#		A[i*Ny+j][i*Ny+j]=1-2.*(lx+ly)
-	#		A[i*Ny+j][i*Ny+(j+1)%Ny] = ly-ay
-	#		A[i*Ny+j][i*Ny+(j-1)%Ny] = ly +ay 
-	#		A[i*Ny+j][((i+1)%Nx)*Ny+j] = lx-ax
-	#		A[i*Ny+j][((i-1)%Nx)*Ny+j] = lx+ax
+	#		A[i*Ny+j][i*Ny+(j+1)%Ny] = ly
+	#		A[i*Ny+j][i*Ny+(j-1)%Ny] = ly  
+	#		A[i*Ny+j][((i+1)%Nx)*Ny+j] = lx
+	#		A[i*Ny+j][((i-1)%Nx)*Ny+j] = lx
 	#convert u_arr to u_vec
 	#u_vec = matrixToVec(u_arr,Nx,Ny)
 	##Time loop
@@ -133,21 +141,22 @@ for k in range(0,3):
 		#print repr(t)
 		#update u_vec
 		#u_vec = np.dot(A,u_vec)
-		u_old = u_arr
+		u_old = [row[:] for row in u_arr]
 		for i in range(0,Nx):
 			for j in range(0,Ny):
-				u_arr[i][j]=u_old[i][j]*(1.-2.*(lx + ly)) + u_old[(i-1)%Nx][j]*(lx + ax)+u_old[(i+1)%Nx][j]*(lx - ax)+u_old[i][(j-1)%Ny]*(ly + ay)+u_old[i][(j+1)%Ny]*(ly - ay)
-					    
+				u_arr[i][j]=u_old[i][j]*(1.-2.*(lx + ly)) + u_old[period(i-1,Nx)][j]*(lx+ax) +u_old[period(i+1,Nx)][j]*(lx-ax)+u_old[i][period(j-1,Ny)]*(ly+ay)+u_old[i][period(j+1,Ny)]*(ly-ay)
+
+        print repr(u_arr[Nx-1][Ny-1])			    
 	#u_arr=vecToMatrix(u_vec,Nx,Ny)
 	##Calculate the error
 	
 	f = [[0 for i in xrange(Ny)] for i in xrange(Nx)]
 	for i in range(0,Nx):
 		for j in range(0,Ny):
-			f[i][j] =u_arr[i][j]- u_exact(i*dx,j*dy,t,Pe)
+			f[i][j] =u_arr[i][j]- u_exact(i*dx,j*dy,t,Pe,alpha)
 			f[i][j]=f[i][j]**2
-
-        err=errorL2(f,dx,dy,Nx,Ny)
+	
+        err=sqrt(errorL2(f,dx,dy,Nx,Ny))
 	error.append(err)
 	print 'Calculate the error  for ' + repr(Nx) + ' : ' + repr(err)
 
